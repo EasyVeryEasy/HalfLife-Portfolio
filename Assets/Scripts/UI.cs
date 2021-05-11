ca5452844a3cee43e88a3ccd4bfa7f28d08b2830
EasyVeryEasy <wlgns7010@naver.com>
2021-05-11 17:00:41 +0900

    ui 시작

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    Text healthText;
+
+    private void Start()
+    {
+        healthText = GetComponent<Text>();
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+}
